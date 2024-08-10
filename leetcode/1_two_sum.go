dab7c7585fecab03a14d1878b7e7c0f387b91710
Minh Dinh <minh.dtv@teko.vn>
2024-08-10 18:02:49 +0700

    leet code two-sum solution

---
+++ leetcode/1_two_sum.go
@@ -0,0 +1,16 @@
+package leetcode
+
+// twoSum uses hashmap with one pass to solve the problem
+func twoSum(nums []int, target int) []int {
+	args := make(map[int]int)
+	res := make([]int, 2)
+
+	for idx1, vl1 := range nums {
+		if idx2, ok := args[target-vl1]; ok {
+			res[0] = idx1
+			res[1] = idx2
+		}
+		args[vl1] = idx1
+	}
+	return res
+}
