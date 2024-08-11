cf1c408709b79a4f56600b76377fa696a82a90fe
Minh Dinh <minh.dtv@teko.vn>
2024-08-11 12:57:51 +0700

    leet code palindrome-number solution

---
+++ leetcode/9_palindrome_number.go
@@ -0,0 +1,17 @@
+package leetcode
+
+// isPalindrome reverses the entire number x, then check if new number is equal to the original number
+// problem: https://leetcode.com/problems/palindrome-number/description/
+// TODO: solve by reversing half of the number x
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	clone := x
+	y := 0
+	for x != 0 {
+		y = y*10 + x%10
+		x /= 10
+	}
+	return clone == y
+}
---
+++ leetcode/9_palindrome_number_test.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				x: 121,
+			},
+			want: true,
+		},
+		{
+			name: "test case 2",
+			args: args{
+				x: -121,
+			},
+			want: false,
+		},
+		{
+			name: "test case 3",
+			args: args{
+				x: 10,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, isPalindrome(tt.args.x), "isPalindrome(%v)", tt.args.x)
+		})
+	}
+}
