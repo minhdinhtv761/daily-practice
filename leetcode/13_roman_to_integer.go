4cc1b95e1d325bc7e536c8281b3770c0ac1f5326
Minh Dinh <vanminhpson727@gmail.com>
2024-08-13 20:42:33 +0700

    leet code roman-to-integer solution

---
+++ leetcode/13_roman_to_integer.go
@@ -0,0 +1,26 @@
+package leetcode
+
+// romanToInt uses hashmap to calculate the convert roman to int
+// problem: https://leetcode.com/problems/roman-to-integer/description/
+func romanToInt(s string) int {
+	symbol2ValueMap := map[int32]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+	tmp := 0
+	for _, c := range s {
+		v := symbol2ValueMap[c]
+		result += v
+		if tmp < v {
+			result -= 2 * tmp
+		}
+		tmp = v
+	}
+	return result
+}
---
+++ leetcode/13_roman_to_integer_test.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_romanToInt(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				s: "III",
+			},
+			want: 3,
+		},
+		{
+			name: "test case 2",
+			args: args{
+				s: "LVIII",
+			},
+			want: 58,
+		},
+		{
+			name: "test case ",
+			args: args{
+				s: "MCMXCIV",
+			},
+			want: 1994,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, romanToInt(tt.args.s), "romanToInt(%v)", tt.args.s)
+		})
+	}
+}
