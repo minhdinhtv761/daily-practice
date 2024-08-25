380e2e3c6e3d25afa0879b4a5d48c0a078120603
Minh Dinh <vanminhpson727@gmail.com>
2024-08-25 19:02:35 +0700

    remove global pkg

--- leetcode/easy/21_merge_two_sorted_lists_test.go
+++ leetcode/easy/21_merge_two_sorted_lists_test.go
@@ -1,7 +1,6 @@
 package easy
 
 import (
-	"daily-practice/leetcode/global"
 	"daily-practice/models"
 	"testing"
 
@@ -25,7 +24,6 @@ func Test_mergeTwoLists(t *testing.T) {
 				list1: models.NewListNode([]int{1, 2, 4}),
 				list2: models.NewListNode([]int{1, 3, 4}),
 			},
-			mode: global.SolutionModeRecursive,
 			want: []int{1, 1, 2, 3, 4, 4},
 		},
 		{
@@ -34,7 +32,6 @@ func Test_mergeTwoLists(t *testing.T) {
 				list1: models.NewListNode([]int{}),
 				list2: models.NewListNode([]int{}),
 			},
-			mode: global.SolutionModeRecursive,
 			want: []int{},
 		},
 		{
@@ -43,13 +40,11 @@ func Test_mergeTwoLists(t *testing.T) {
 				list1: models.NewListNode([]int{}),
 				list2: models.NewListNode([]int{0}),
 			},
-			mode: global.SolutionModeRecursive,
 			want: []int{0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			global.SetSolutionMode(tt.mode)
 			assert.Equalf(t, tt.want, mergeTwoLists(tt.args.list1, tt.args.list2).GetVals(), "mergeTwoLists(%v, %v)", tt.args.list1, tt.args.list2)
 		})
 	}
